515803be0449cb47016169f4ebf8b08c961776ef
Tom Witkowski <dev.gummibeer@gmail.com>
2020-02-18 11:26:33 +0100

    improve service provider

--- src/TrashmailRuleServiceProvider.php
+++ src/TrashmailRuleServiceProvider.php
@@ -8,23 +8,34 @@ class TrashmailRuleServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->bootConfig();
+            $this->bootLang();
+        }
+    }
+
+    public function register(): void
+    {
+        $this->app->singleton(TrashmailManager::class);
+
+        $this->app->singleton(Trashmail::class);
+    }
+
+    protected function bootConfig(): void
+    {
         $this->publishes([
             __DIR__.'/../config/trashmail.php' => config_path('trashmail.php'),
         ], 'config');
 
         $this->mergeConfigFrom(__DIR__.'/../config/trashmail.php', 'trashmail');
+    }
 
+    protected function bootLang(): void
+    {
         $this->publishes([
             __DIR__.'/../resources/lang' => resource_path('lang/vendor/trashmailRule'),
         ], 'lang');
 
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'trashmailRule');
-    }
-
-    public function register()
-    {
-        $this->app->singleton(TrashmailManager::class);
-
-        $this->app->singleton(Trashmail::class);
     }
 }
